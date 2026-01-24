265d01db0e5bdb5b0e63328cabe74de96f738258
Faheem <faheempktl@gmail.com>
2026-01-24 12:19:53 +0530

    Create customer service implemented

---
+++ services/customer/src/main/java/com/faheem/ecommerce/customer/CustomerRequest.java
@@ -0,0 +1,22 @@
+package com.faheem.ecommerce.customer;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record CustomerRequest(
+         String id,
+
+         @NotNull(message = "Customer firstname is required")
+         String firstName,
+
+         @NotNull(message = "Customer lastname is required")
+         String lastName,
+
+         @NotNull(message = "Customer email is required")
+         @Email(message = "email is not valid")
+         String email,
+
+         Address address
+) {
+}
---
+++ services/customer/src/main/java/com/faheem/ecommerce/customer/CustomerService.java
@@ -0,0 +1,16 @@
+package com.faheem.ecommerce.customer;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepository repository;
+    private final CustomerMapper mapper;
+    public String createCustomer(@Valid CustomerRequest request) {
+        var customer = repository.save(mapper.toCustomer(request));
+        return customer.getId();
+    }
+}
