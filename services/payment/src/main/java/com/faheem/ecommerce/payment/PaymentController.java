d8e5d83bee5382625d8238e8e568103cb4a4947e
Faheem <faheempktl@gmail.com>
2026-02-01 10:43:16 +0530

    Payment entity implemented

---
+++ services/payment/src/main/java/com/faheem/ecommerce/payment/PaymentController.java
@@ -0,0 +1,7 @@
+package com.faheem.ecommerce.payment;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+}
---
+++ services/payment/src/main/java/com/faheem/ecommerce/payment/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.faheem.ecommerce.payment;
+
+public enum PaymentMethod {
+    PAYPAL,
+    CREDITCARD,
+    VISA,
+    MASTER_CARD,
+    BITCOIN
+}
