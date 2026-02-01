fba73d63a0d489ddd83f58886f739da254c8ac4e
Faheem <faheempktl@gmail.com>
2026-02-01 10:57:35 +0530

    payment controller,repo implemented

---
+++ services/payment/src/main/java/com/faheem/ecommerce/payment/Customer.java
@@ -0,0 +1,20 @@
+package com.faheem.ecommerce.payment;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+public record Customer(
+        String id,
+        @NotNull(message = "First name is required")
+        String firstName,
+
+        @NotNull(message = "Last name is required")
+        String lastName,
+
+        @NotNull(message = "Email is required")
+        @Email(message = "Incorrect email format")
+        String email
+) {
+}
---
+++ services/payment/src/main/java/com/faheem/ecommerce/payment/PaymentService.java
@@ -0,0 +1,17 @@
+package com.faheem.ecommerce.payment;
+
+import com.faheem.ecommerce.PaymentApplication;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+    private final PaymentRepository repository;
+    private final paymentMapper mapper;
+
+//    public Integer createPayment(PaymentRequest request) {
+//        var payment = repository.save(mapper.toPayment(request));
+//
+//    }
+}
