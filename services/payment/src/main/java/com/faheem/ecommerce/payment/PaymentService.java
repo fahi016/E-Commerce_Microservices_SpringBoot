ec730f419ad6ef21ac50462b62acbef536363840
Faheem <faheempktl@gmail.com>
2026-02-01 11:32:16 +0530

    Payment configuration implemented

--- services/payment/src/main/java/com/faheem/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/com/faheem/ecommerce/payment/PaymentService.java
@@ -1,18 +1,33 @@
 package com.faheem.ecommerce.payment;
 
 import com.faheem.ecommerce.PaymentApplication;
+import com.faheem.ecommerce.notification.NotificationProducer;
+import com.faheem.ecommerce.notification.PaymentNotificationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.management.Notification;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
     private final PaymentRepository repository;
     private final paymentMapper mapper;
+    private final NotificationProducer notificationProducer;
 
     public Integer createPayment(PaymentRequest request) {
         var payment = repository.save(mapper.toPayment(request));
-        return null;
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstName(),
+                        request.customer().lastName(),
+                        request.customer().email()
+                )
+        );
+        return payment.getId();
 
     }
 }
